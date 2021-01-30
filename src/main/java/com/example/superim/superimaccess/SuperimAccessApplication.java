f2ec979613d90da9f4cf38814c43e05c88f9a65f
lwwwl <lwggcn@gmail.com>
2021-01-30 17:32:00 +0800

    SuperIM接入服务-空项目

---
+++ src/main/java/com/example/superim/superimaccess/SuperimAccessApplication.java
@@ -0,0 +1,13 @@
+package com.example.superim.superimaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperimAccessApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SuperimAccessApplication.class, args);
+    }
+
+}
